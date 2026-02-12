6646193c8be3d945ea0d1717b4961295bc9f4cf4
SoMSoM <som.emonkh@gmail.com>
2026-02-12 13:36:08 +0700

    update

--- jest.setup.js
+++ jest.setup.js
@@ -1,12 +1,7 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
-
 jest.mock('react-native-reanimated', () => {
-  const mocks = jest.requireActual(
-    './node_modules/react-native-reanimated/mock'
-  );
+  const mocks = jest.requireActual('react-native-reanimated/mock');
   return {
     ...mocks,
-    defineAnimation: () => {},
-    createAnimatedPropAdapter: () => {},
+    createAnimatedPropAdapter: (cb) => cb, 
   };
 });
